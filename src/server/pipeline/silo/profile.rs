f0b0d2f53a519ff6d03fe2d7c3ac49025ff80b7b
aegroto <aegroto@protonmail.com>
2022-01-24 14:50:49 +0100

    server profiler thread code refactoring

--- src/server/pipeline/silo/profile.rs
+++ src/server/pipeline/silo/profile.rs
@@ -10,10 +10,13 @@ use tokio::{
     task::JoinHandle,
 };
 
-use crate::{common::{feedback::FeedbackMessage, helpers::silo::channel_pull}, server::{
-        profiling::{TransmissionRoundStats, TransmittedFrameStats, ServerProfiler},
+use crate::{
+    common::{feedback::FeedbackMessage, helpers::silo::channel_pull},
+    server::{
+        profiling::{ServerProfiler, TransmissionRoundStats, TransmittedFrameStats},
         utils::profilation::setup_round_stats,
-    }};
+    },
+};
 
 use super::transfer::TransferResult;
 
@@ -29,26 +32,48 @@ pub fn launch_profile_thread(
         let mut round_stats = setup_round_stats(csv_profiling, console_profiling).unwrap();
 
         loop {
-            let (transfer_result, total_time) = channel_pull(&mut transfer_result_receiver)
-                .await
-                .expect("Transfer result channel closed, terminating.");
+            let (transfer_result, total_time) =
+                pull_transfer_result(&mut transfer_result_receiver).await;
 
             let mut frame_stats = transfer_result.frame_stats;
             frame_stats.total_time = total_time;
 
-            round_stats.profile_frame(frame_stats);
+            profile(&mut round_stats, frame_stats, round_duration);
 
-            let current_round_duration = round_stats.start_time.elapsed();
-
-            if current_round_duration.gt(&round_duration) {
-                round_stats.log();
-                round_stats.reset();
-            }
-
-            while let Some(message) = profiler.pull_feedback().await {
-                feedback_sender.send(message)
-                    .expect("Unable to broadcast a feedback message");
-            }
+            broadcast_feedbacks(&mut profiler, &feedback_sender).await;
         }
     })
+}
+
+fn profile(
+    round_stats: &mut TransmissionRoundStats,
+    frame_stats: TransmittedFrameStats,
+    round_duration: Duration,
+) {
+    round_stats.profile_frame(frame_stats);
+    let current_round_duration = round_stats.start_time.elapsed();
+    if current_round_duration.gt(&round_duration) {
+        round_stats.log();
+        round_stats.reset();
+    }
+}
+
+async fn broadcast_feedbacks(
+    profiler: &mut Box<dyn ServerProfiler + Send>,
+    feedback_sender: &Sender<FeedbackMessage>,
+) {
+    while let Some(message) = profiler.pull_feedback().await {
+        feedback_sender
+            .send(message)
+            .expect("Unable to broadcast a feedback message");
+    }
+}
+
+async fn pull_transfer_result(
+    transfer_result_receiver: &mut UnboundedReceiver<TransferResult>,
+) -> (TransferResult, u128) {
+    let (transfer_result, total_time) = channel_pull(transfer_result_receiver)
+        .await
+        .expect("Transfer result channel closed, terminating.");
+    (transfer_result, total_time)
 }
