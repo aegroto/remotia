a8afbe6fb8208dd5a47b0097161be083b882ce74
aegroto <aegroto@protonmail.com>
2022-02-09 16:58:09 +0100

    srt examples refactoring

--- remotia-samples/examples/srt_client/main.rs
+++ remotia-samples/examples/srt_client/main.rs
@@ -1,6 +1,4 @@
-use remotia::{
-    server::pipeline::ascode::{component::Component, AscodePipeline},
-};
+use remotia::server::pipeline::ascode::{component::Component, AscodePipeline};
 use remotia_buffer_utils::BufferAllocator;
 use remotia_core_renderers::beryllium::BerylliumRenderer;
 use remotia_ffmpeg_codecs::decoders::h264::H264Decoder;
@@ -12,36 +10,21 @@ async fn main() -> std::io::Result<()> {
 
     let width = 1280;
     let height = 720;
+    let buffer_size = width * height * 4;
 
     // Pipeline structure
-    let receive_component = {
-        let buffer_size = width * height * 4;
-        let receiver = SRTFrameReceiver::new("127.0.0.1:5001").await;
-
-        Component::new()
-            .add(BufferAllocator::new("encoded_frame_buffer", buffer_size))
-            .add(receiver)
-    };
-
-    let decode_component = {
-        let buffer_size = width * height * 4;
-        let decoder = H264Decoder::new();
-
-        Component::new()
-            .add(BufferAllocator::new("raw_frame_buffer", buffer_size))
-            .add(decoder)
-    };
-
-    let render_component = {
-        let renderer = BerylliumRenderer::new(width as u32, height as u32);
-
-        Component::new().add(renderer)
-    };
-
     let pipeline = AscodePipeline::new()
-        .add(receive_component)
-        .add(decode_component)
-        .add(render_component);
+        .add(
+            Component::new()
+                .add(BufferAllocator::new("encoded_frame_buffer", buffer_size))
+                .add(SRTFrameReceiver::new("127.0.0.1:5001").await),
+        )
+        .add(
+            Component::new()
+                .add(BufferAllocator::new("raw_frame_buffer", buffer_size))
+                .add(H264Decoder::new()),
+        )
+        .add(Component::new().add(BerylliumRenderer::new(width as u32, height as u32)));
 
     pipeline.run().await;
 
--- remotia-samples/examples/srt_server/main.rs
+++ remotia-samples/examples/srt_server/main.rs
@@ -1,8 +1,6 @@
 use std::time::Duration;
 
-use remotia::server::{
-    pipeline::ascode::{component::Component, AscodePipeline},
-};
+use remotia::server::pipeline::ascode::{component::Component, AscodePipeline};
 use remotia_buffer_utils::BufferAllocator;
 use remotia_core_capturers::scrap::ScrapFrameCapturer;
 use remotia_ffmpeg_codecs::encoders::h264::H264Encoder;
@@ -13,40 +11,24 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     // Pipeline structure
-
-    let (width, height, capture_component) = {
-        let capturer = ScrapFrameCapturer::new_from_primary();
-        let width = capturer.width();
-        let height = capturer.height();
-        let buffer_size = width * height * 4;
-
-        let component = Component::new()
-            .with_tick(33)
-            .add(BufferAllocator::new("raw_frame_buffer", buffer_size))
-            .add(capturer);
-
-        (width, height, component)
-    };
-
-    let encode_component = {
-        let buffer_size = width * height * 4;
-        let encoder = H264Encoder::new(buffer_size, width as i32, height as i32);
-
-        Component::new()
-            .add(BufferAllocator::new("encoded_frame_buffer", buffer_size))
-            .add(encoder)
-    };
-
-    let transmission_component = {
-        let sender = SRTFrameSender::new(5001, Duration::from_millis(50)).await;
-
-        Component::new().add(sender)
-    };
+    let capturer = ScrapFrameCapturer::new_from_primary();
+    let width = capturer.width();
+    let height = capturer.height();
+    let buffer_size = width * height * 4;
 
     let pipeline = AscodePipeline::new()
-        .add(capture_component)
-        .add(encode_component)
-        .add(transmission_component);
+        .add(
+            Component::new()
+                .with_tick(33)
+                .add(BufferAllocator::new("raw_frame_buffer", buffer_size))
+                .add(capturer),
+        )
+        .add(
+            Component::new()
+                .add(BufferAllocator::new("encoded_frame_buffer", buffer_size))
+                .add(H264Encoder::new(buffer_size, width as i32, height as i32)),
+        )
+        .add(Component::new().add(SRTFrameSender::new(5001, Duration::from_millis(50)).await));
 
     pipeline.run().await;
 
